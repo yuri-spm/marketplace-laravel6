f4f87816655dbdc4a5860a32532fab71c982ca48
yuri-spm <63649751+yuri-spm@users.noreply.github.com>
2021-08-09 17:20:56 -0300

    Melhorias Painel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,37 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="margin-bottom: 40px">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="{{route('home')}}">Marketplace L6</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item @if(request()->is('admin/stores')) active @endif">
+                    <a class="nav-link" aria-current="page" href="{{route('admin.stores.index')}}">Lojas</a>
+                </li>
+                <li class="nav-item  @if(request()->is('admin/products')) active @endif">
+                    <a class="nav-link" href="{{route('admin.products.index')}}">Produtos</a>
+                </li>
+            </ul>
+            <div action="" class="my-2 mylg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#" onclick="event.preventDefault();
+                                                                                  document.querySelector('form.logout').submit();">Sair</a>
+                        <form action="{{route('logout')}}" class="logout" method="POST">
+                            @csrf
+                        </form>
+                    </li>
+
+                </ul>
+            </div>
+
+        </div>
+    </div>
+</nav>
     <div class="container">
         @include('flash::message')
         @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -13,25 +13,27 @@
 
 Route::get('/', function () {
     return view('welcome');
+})->name('home');
+
+Route::group(['middleware' => ['auth']], function (){
+    Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function (){
+        // Route::prefix('stores')->name('stores.')->group(function (){
+        //     Route::get('/', 'StoreController@index')->name('index');
+        //     Route::get('/create', 'StoreController@create')->name('create');
+        //     Route::post('/store', 'StoreController@store')->name('store');
+        //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
+        //     Route::post('/update/{store}', 'StoreController@update')->name('update');
+        //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
+
+        // });
+
+        Route::resource('stores', 'StoreController');
+        Route::resource('products', 'ProductController');
+
+    });
+
 });
-
-Route::prefix('admin')->name('admin.')->namespace('Admin')->group(function (){
-    // Route::prefix('stores')->name('stores.')->group(function (){
-    //     Route::get('/', 'StoreController@index')->name('index');
-    //     Route::get('/create', 'StoreController@create')->name('create');
-    //     Route::post('/store', 'StoreController@store')->name('store');
-    //     Route::get('/{store}/edit', 'StoreController@edit')->name('edit');
-    //     Route::post('/update/{store}', 'StoreController@update')->name('update');
-    //     Route::get('/destroy/{store}', 'StoreController@destroy')->name('destroy');
-
-    // });
-
-    Route::resource('stores', 'StoreController');
-    Route::resource('products', 'ProductController');
-
-});
-
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
