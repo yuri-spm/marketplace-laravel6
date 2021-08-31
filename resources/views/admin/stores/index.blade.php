e4c3f6e59028f0110a54e8f097f8137588b1afdf
Yuri do Monte <yuri.spm@gmail.com>
2021-08-30 22:58:45 -0300

    Melhorias em lojas

--- resources/views/admin/stores/index.blade.php
+++ resources/views/admin/stores/index.blade.php
@@ -2,39 +2,37 @@
 
 @section('content')
     @if (!$store)
-
         <a href="{{ route('admin.stores.create') }}" class="btn btn-lg btn-success">Criar Loja</a>
+    @else
+        <table class="table table-str">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Loja</th>
+                    <th>Total de Produtos</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
 
+                    <td>{{ $store->id }}</td>
+                    <td>{{ $store->name }}</td>
+                    <td>{{ $store->products->count() }}</td>
+                    <td>
+                        <div class="btn-group">
+                            <a href="{{ route('admin.stores.edit', ['store' => $store->id]) }}"
+                                class="btn btn-sm btn-primary">Editar</a>
+                            <form action="{{ route('admin.stores.destroy', ['store' => $store->id]) }}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-sm btn-danger ">Excluir</button>
+                            </form>
+                        </div>
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
     @endif
-    <table class="table table-str">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Loja</th>
-                <th>Total de Produtos</th>
-                <th>Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-
-                <td>{{ $store->id }}</td>
-                <td>{{ $store->name }}</td>
-                <td>{{ $store->products->count() }}</td>
-                <td>
-                    <div class="btn-group">
-                        <a href="{{ route('admin.stores.edit', ['store' => $store->id]) }}"
-                            class="btn btn-sm btn-primary">Editar</a>
-                        <form action="{{ route('admin.stores.destroy', ['store' => $store->id]) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-sm btn-danger ">Excluir</button>
-                        </form>
-                    </div>
-                </td>
-
-            </tr>
-        </tbody>
-    </table>
-
 @endsection
