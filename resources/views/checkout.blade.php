97995d106705c62cf7a6ebdc179b7a4ddcf09850
Yuri do Monte <yuri.spm@gmail.com>
2021-09-17 23:05:15 -0300

    Bandeira cartao p1

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -53,17 +53,37 @@
 @section('scripts')
 
     <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
+
     <script>
-        cont sessionId = '{{ session()->get('pagseguro_session_code') }}';
+
+        const sessionId = '{{session()->get('pagseguro_session_code')}}';
 
         PagSeguroDirectPayment.setSessionId(sessionId);
+
     </script>
 
     <script>
+
         let cardNumber = document.querySelector('input[name=card_number]');
+
         cardNumber.addEventListener('keyup', function(){
-            console.log(cardNumber.value);
+            if(cardNumber.value.length >= 6) {
+                PagSeguroDirectPayment.getBrand({
+                    cardBin: cardNumber.value.substr(0, 6),
+                    success: function(res) {
+                        console.log(res);
+                    },
+                    error: function(err){
+                        console.log(err);
+                    },
+                    complete: function(res){
+                        console.log('Complete', res);
+                    }
+                });
+
+            }
         });
+
     </script>
 
 @endsection
